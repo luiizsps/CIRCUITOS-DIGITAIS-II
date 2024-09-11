5b847fdd01340e86c1fbcc4b91fd70425c4be3c7
Luiz Passos <lsps1019@gmail.com>
2024-09-11 11:45:15 -0300

    Create design.sh

---
+++ aula_ 1/design.sh
@@ -0,0 +1,16 @@
+// Code your design here
+// C = 2A - B
+module produto (
+  input [1:0] A,
+  input [1:0] B,
+  output reg [2:0] C);
+  
+  wire [2:0] D;
+  assign D = 2'd2*A;
+  assign C = D - B;
+  
+  always @ (*) begin
+  	c = D - B;
+  end
+
+endmodule
