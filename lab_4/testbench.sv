af4593c60352eaef333fc6b4fb4d734333b06eb4
Luiz Passos <lsps1019@gmail.com>
2024-11-03 23:47:47 -0300

    Update testbench.sv

--- lab_4/testbench.sv
+++ lab_4/testbench.sv
@@ -1,9 +1,7 @@
-// Code your testbench here
-// or browse Examples
 module fsm;
   
-  reg clk, rst_n, enter, keyA, keyB, OPEN;
-  wire clk_div, sinc_enter, ERROR, LED;
+  reg clk, rst_n, enter, keyA, keyB;
+  wire clk_div, sinc_enter, ERROR, LED, OPEN;
    	
   divisor_de_clock u0 (.clk(clk), .rst_n(rst_n), .clk_div(clk_div));
   sincronizador_enter_clock u1 (.clk_div(clk_div), .rst_n(rst_n), .enter(enter), .sinc_enter(sinc_enter));
@@ -12,31 +10,56 @@ module fsm;
   
   initial begin
     clk = 0;
-    while (1) begin
-      #10;
-      clk = ~clk;
-    end
+    forever #10 clk = ~clk;
   end
 
   initial begin
     $dumpfile("dump.vcd");
-    $dumpvars;
+    $dumpvars(0, fsm);
+    
     rst_n = 0;
+    keyA = 0;
+    keyB = 0;
+    enter = 0;
     @(negedge clk);
     rst_n = 1;
-    keyB = 1;
+    
     keyA = 1;
+    keyB = 0;
     enter = 1;
-    repeat(5) @(negedge clk);
+    @(negedge clk);
     enter = 0;
-    repeat(10000) @(negedge clk);
+    repeat(100) @(negedge clk);
+
+    keyA = 0;
     keyB = 1;
-    keyA = 1;
     enter = 1;
-    repeat(5) @(negedge clk);
+    @(negedge clk);
     enter = 0;
-    repeat(20000) @(negedge clk);
+    repeat(100) @(negedge clk);
+	// OPEN = 1
+    
+    keyA = 1;
+    keyB = 1;
+    enter = 1;
+    @(negedge clk);
+    enter = 0;
+    repeat(100) @(negedge clk);
+    
+    keyA = 1;
+    keyB = 1;
+    enter = 1;
+    @(negedge clk);
+    enter = 0;
+    repeat(100) @(negedge clk);
+	// ERROR = 1
+    
+    enter = 1;
+    @(negedge clk);
+    enter = 0;
+    repeat(100) @(negedge clk);
+    
     $finish;
   end
-
 endmodule
+
